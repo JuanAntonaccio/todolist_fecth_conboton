64f8229cf0df2c7c59813c766f78c45e2a4063dc
JuanAntonaccio <jmantonaccio@gmail.com>
2021-09-18 01:03:44 +0000

    casi terminado me falta borrar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,58 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const Home = () => {
+	const [tarea, setTarea] = useState("");
+	const [lista, setLista] = useState([]);
+
+	const cargarDato = event => {
+		setTarea(event.target.value);
+	};
+
+	const enviarDatos = event => {
+		event.preventDefault();
+		setLista([...lista, tarea]);
+		setTarea("");
+	};
+
+	const [eventos, setEventos] = useState([]);
+
+	const eliminar = {
+		    
+			//SetLista(lista.filter(el => el !== index))
+			//const filtredData = this.state.items.filter(item => item.id !== itemId);
+		}
+	}
+
 	return (
-		<div className="container">
-			
+		<div className="container mt-10">
+			<h1>Todos</h1>
+			<form className="row" onSubmit={enviarDatos}>
+				<div className="col-md-3">
+					<input
+						placeholder="Ingrese Tarea"
+						className="form-control"
+						type="text"
+						value={tarea}
+						onChange={cargarDato}></input>
+				</div>
+
+				<div className="col-md-3">
+					<button className="btn btn-primary" type="submit">
+						Enviar
+					</button>
+				</div>
+			</form>
+			<ul className="list-group">
+				{lista.map((item, index) => (
+					<li
+						key={index}
+						className="list-group-item"
+						onMouseOver={() => setEventos(index)}>
+						{item}
+						{eventos == index ? <button onClick={eliminar}>X</button> : ""}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
